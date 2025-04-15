bc81a54a8bd5459926926f766838b5f449bb409d
Archie-f <akif.mk66@gmail.com>
2025-04-15 23:14:05 +0200

    Wiring beans using Autowired annotation on class fields.

--- example11-wiring-beans-using-autowired-annotation-on-class-fields/src/main/java/com/example/main/Example11.java
+++ example11-wiring-beans-using-autowired-annotation-on-class-fields/src/main/java/com/example/main/Example11.java
@@ -10,7 +10,7 @@ public class Example11 {
     public static void main(String[] args) {
 
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
-        Person person = context.getBean(Person.class);
+        Person person = context.getBean(Person.class);//This will not print "Person bean created by Spring" text because constructor ıs not generated.
         Vehicle vehicle = context.getBean(Vehicle.class);
         System.out.println("Person name from Spring Context : " + person.getName());
         System.out.println("Vehicle name from Spring Context : " + vehicle.getName());
